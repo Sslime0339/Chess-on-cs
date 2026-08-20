9f05fcdf46994464453b611ba164a52431b9b8b5
Sslime0339 <sslime0339@gmail.com>
2026-08-20 12:17:33 +0300

    добавил Vector2

---
+++ ChessApp/Chess/Vector2.cs
@@ -0,0 +1,20 @@
+
+
+
+namespace Chess
+{
+  struct Vector2
+  {
+    public int X;
+    public int Y;
+    
+    public Vector2(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+    
+    public static Vector2 operator +(Vector2 a, Vector2 b) => new Vector2(a.X + b.X, a.Y + b.Y);
+    public static Vector2 operator -(Vector2 a, Vector2 b) => new Vector2(a.X - b.X, a.Y - b.Y);
+  }
+}
