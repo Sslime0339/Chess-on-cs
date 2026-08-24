3282ec53b047bf931e4d26f02b08159494695aba
Sslime0339 <Sslime0339@gmail.com>
2026-08-24 15:05:23 +0300

    настройка консоли для Visual Studio

--- ChessApp/Program.cs
+++ ChessApp/Program.cs
@@ -4,27 +4,30 @@ using ConsoleUI;
 
 class Program
 {
-  static void Main()
-  {
-    
-    GameStart.Start();
-    /*
-    // всякоя для UI
-    Console.BackgroundColor = ConsoleColor.White;
-    Console.ForegroundColor = ConsoleColor.Black;
-    //Console.Clear(); // Обязательно для заливки всей консоли цветом
-    
-    Console.WriteLine("\nбелые♔♕♖♗♘♙  \nчёрные♚♛♜♝♞♟");
-    Console.WriteLine("белые ◻▢\nчёрные ◼⬛ddjejue");
-    Console.WriteLine("Привет! Программа на C# успешно работает в Termux!\nВведите любой текст");
-    Console.ResetColor();
-    
-    string str = Console.ReadLine();
-    Console.WriteLine($"твоя строка: {str}");
-    Console.WriteLine("Введите число");
-    str = Console.ReadLine();
-    int a = Convert.ToInt32(str);
-    Console.WriteLine($"число в квадрате\n{a} = {a*a}");
-    */
-  }
+    static void Main()
+    {
+
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        GameStart.Start();
+        /*
+        // всякоя для UI
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        //Console.Clear(); // Обязательно для заливки всей консоли цветом
+
+        Console.WriteLine("\nбелые♔♕♖♗♘♙  \nчёрные♚♛♜♝♞♟");
+        Console.WriteLine("белые ◻▢\nчёрные ◼⬛ddjejue");
+        Console.WriteLine("Привет! Программа на C# успешно работает в Termux!\nВведите любой текст");
+        Console.ResetColor();
+
+        string str = Console.ReadLine();
+        Console.WriteLine($"твоя строка: {str}");
+        Console.WriteLine("Введите число");
+        str = Console.ReadLine();
+        int a = Convert.ToInt32(str);
+        Console.WriteLine($"число в квадрате\n{a} = {a*a}");
+        */
+    }
 }
